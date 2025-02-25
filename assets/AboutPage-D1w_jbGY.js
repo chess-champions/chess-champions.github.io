f7ea5bcba52b16b4bf233c628faa97783f53ec2b
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2025-02-25 15:13:06 +0000

    v 2.6

---
+++ assets/AboutPage-D1w_jbGY.js
@@ -0,0 +1,1 @@
+import{j as t,B as r,T as o}from"./index-CrykEkJo.js";const s=()=>t.jsxs(r,{padding:{xs:2,md:2},children:[t.jsx(o,{variant:"h4",gutterBottom:!0,children:"About Us"}),t.jsx(o,{variant:"body1",children:"Welcome to our application!"})]});export{s as default};
