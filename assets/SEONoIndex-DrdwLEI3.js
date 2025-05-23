fc3622023cd5622738f378581daa8090cb71e189
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2025-05-23 11:09:49 +0000

    v 26.4

--- assets/SEONoIndex-DrdwLEI3.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BP74TvEP.js";import{H as t}from"./ChessDataService-D8pFmC03.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
---
+++ assets/SEONoIndex-FQppFbFD.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DXV6n89O.js";import{H as t}from"./ChessDataService-C2eBtv_s.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
