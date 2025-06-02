907502fd9ecce32b65a6a04a45cbc3afca57ae85
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2025-06-02 14:11:44 +0000

    v 29.3

---
+++ assets/SEONoIndex-BM7PBOqi.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BZxrmeqa.js";import{H as t}from"./ChessDataService-YoTNwGV3.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
