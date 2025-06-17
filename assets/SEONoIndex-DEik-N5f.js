02e10a86c7c0d21b38828de08da1356186f545cc
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2025-06-17 13:32:53 +0000

    v 32.1

---
+++ assets/SEONoIndex-DEik-N5f.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Cvnk9lWS.js";import{H as t}from"./ChessDataService-CnaSWIQF.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
