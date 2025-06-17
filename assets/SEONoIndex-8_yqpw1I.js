dfda7276411051ba5191908a7aac709b9b2667aa
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2025-06-17 11:10:20 +0000

    v 32.0

---
+++ assets/SEONoIndex-8_yqpw1I.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BDoy4eof.js";import{H as t}from"./ChessDataService-CJpW5uYf.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
--- assets/SEONoIndex-ILxUa-3t.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CZfoEV0E.js";import{H as t}from"./ChessDataService-CfjVutSP.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
