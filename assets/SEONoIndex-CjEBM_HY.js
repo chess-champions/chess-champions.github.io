bd50afab5ae9cb05c1368dc35055e60c82d0cdf5
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2025-06-26 21:38:02 +0000

    v 34.3

--- assets/SEONoIndex-CjEBM_HY.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Cqrn6X1o.js";import{H as t}from"./ChessDataService-Dgi5R-O6.js";const r=()=>o.jsx(t,{children:o.jsx("meta",{name:"robots",content:"noindex"})});export{r as S};
